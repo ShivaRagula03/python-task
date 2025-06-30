07a92d770d7daa5181552f536f0e4e05ad73c2af
Shiva Ragula <120513143+ShivaRagula03@users.noreply.github.com>
2025-06-30 19:52:41 +0530

    Add files via upload

--- task.py
+++ task.py
@@ -1,84 +1,38 @@
-# *        *
-# *  *     *
-# *    *   *
-# *     *  *
-# *        *
-rows = 5
-for i in range(1,rows+1):
-    res=""
-    for j in range(1,rows+1):
-        if j==1 or j==rows or i==j:
-         res+="*"+" "
+# create a random otp should not contain zeroes
+import random as r
+import math as m
+def random_opt(length):
+    opt=""
+    while len(opt)!=length:
+        digits= m.floor(r.random()*10)
+        if digits!=0:
+            opt=opt+str(digits)
+    return opt
+length_size = int(input("Enter The Length Of OPT : "))
+print("The Generated OPT:",random_opt(length_size))
+
+# guess the number within 3 chances
+numbers = [2,4,6,8,10,12,14]
+given_chances = 3
+number = r.choice(numbers)
+print("Guess a Number Between Numbers List U have Only 3 Chances..!!")#game start
+#
+while given_chances > 0:
+    guess = int(input("\n Guess The Number : ")) 
+    
+    if guess==number:
+        print(f"Congratulations u guessed it correct ...{number}.!!!")
+        break
+    else:
+        given_chances = given_chances - 1
+        if given_chances>0:
+            print(f"Try Again There is only {given_chances} chances  left ...!!")
         else:
-            res+=" "+" "
-    print(res)
-print("\n")
-
-
-# * * * * *
-#       *
-#     *
-#   *
-# * * * * *
-
-
-rows2=5
-for k in range(1,rows2+1):
-    res1=""
-    for l in range(1,rows2 +1):
-     if k == 1 or k == rows2  or k+l == rows2+1:
-        res1+="*"+" "
-     else:
-         res1+=" "+" "
-    print(res1)
-print("\n")
-    
-    
-
-# *        *
-# *        *
-# * * * * *       
-# *        *
-# *        *
-
-
-rows3 = 5
-mid = rows3 // 2 + 1
-for r  in range(1,rows3 + 1):
-     result=""
-     for c in range(1,rows3 + 1):
-         if c == 1 or c == rows3  or r == mid :
-             result += "*"+" "
-         else:
-             result +=" "+" "
-     print(result)
-     
-print("\n")
-    
-    
-    
-    
-    
-    
-# * * * * *
-#     *
-#     *
-#     *
-#     *
-# * * * * *
-
-
-
-rows4 = 5
-mid = rows4 // 2 + 1
-for a in range(1,rows4+1):
-    res1=""
-    for b in range(1,rows4+1):
-        if a == 1 or a == rows or b == mid:
-            res1+="*"+" "
-        else:
-            res1+=" "+" "
-    print(res1)
+            print(f"Oho Sorry Your Chances Is completed ...GameOver...!!the Correct Number is :{number}")
+            
         
-         
-      
+    
+
+
+
+
